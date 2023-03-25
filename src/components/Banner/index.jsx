3eb5a603cfc5bdb4badf4666330a23229511d91c
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-03-25 19:26:02 -0300

    fixed: corrigi um bug

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className={styles.banner}>
       <h1 className={styles.banner__title}>
-        A galeria mais completa do espaço
+        A galeria mais completa de fotos do espaço
       </h1>
       <img src={banner} alt="banner" className={styles.banner__image} />
     </div>
