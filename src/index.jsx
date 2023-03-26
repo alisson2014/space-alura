ad265a75f4d5d58a6017f8d3965f3ceac2e6946a
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-03-26 17:19:47 -0300

    refactor: ajustes nas pastas

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./fonts/GandhiSans-Regular.otf";
-import "./fonts/GandhiSans-Bold.otf";
+import "./styles/fonts/GandhiSans-Regular.otf";
+import "./styles/fonts/GandhiSans-Bold.otf";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
